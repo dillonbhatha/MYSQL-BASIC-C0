a0db9a70b9c2ede0e73f68eae7f58bae95d53659
dillon bhatha <dillon.bhatha@student.rocva.nl>
2022-01-27 19:44:07 +0100

    opdracht 7 afgemaakt

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,17 @@
+-- Opdracht 1
+SELECT * FROM players WHERE id="198329";
+
+
+id    name    age    nationality    club    value    wage
+198329    Rodrigo     26    Spain     Valencia CF     13000000    36000
+198329    Rodrigo     26    Spain     Valencia CF     13000000    36000
+
+DELETE FROM players WHERE id = 198329;
+-- Opdracht 2
+DELETE FROM `players` WHERE id='199304';
 -- Opdracht 3
-
+DELETE FROM `players` WHERE id='207494';
 -- Opdracht 4
-
+DELETE FROM `players` WHERE id='226753';
 -- Opdracht 5
+Uw SQL-query is succesvol uitgevoerd
